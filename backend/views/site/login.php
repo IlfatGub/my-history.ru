d2f5f626ba76bb927c53b4197a4595c8d520d937
Ilfat <ilfat.gubaidullin@gmail.com>
2017-10-22 21:58:08 +0500

    s

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -12,11 +12,8 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
-12121212121212121212121212121212121222222
+
     <p>Please fill out the following fields to login:</p>
-    <p>Please fill out the following fields to login:</p>
-        <p>Please fill out the following fields to login:</p>
-            
 
     <div class="row">
         <div class="col-lg-5">
